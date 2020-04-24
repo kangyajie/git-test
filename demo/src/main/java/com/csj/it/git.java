15354157034b7f4e9cebbed344c08645a5c53439
KYJ <603528438@qq.com>
2020-04-24 21:51:44 +0800

    删除

--- demo/src/main/java/com/csj/it/git.java
+++
@@ -1,8 +0,0 @@
-package com.csj.it;
-
-public class git {
-    public static void main(String[] args) {
-        System.out.println("111111111111");
-        System.out.println("222222222222");
-    }
-}
