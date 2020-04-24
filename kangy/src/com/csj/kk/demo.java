2bf62354839a6c206d87e1182bba3a9bc74c6969
KYJ <603528438@qq.com>
2020-04-24 22:35:03 +0800

    远程的修改

--- kangy/src/com/csj/kk/demo.java
+++ kangy/src/com/csj/kk/demo.java
@@ -4,6 +4,9 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("11111111111");
         //22222
-        //333
+    }
+    public void test(){
+        int a=10;
+        System.out.println(a);
     }
 }
